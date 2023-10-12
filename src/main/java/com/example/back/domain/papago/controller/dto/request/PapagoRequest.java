345ad9ce7df213f87ba1b51719fb1487dabde540
min56 <minchaeg85@gmail.com>
2023-10-12 10:03:29 +0900

    📑 :: PapagoRequest

---
+++ src/main/java/com/example/back/domain/papago/controller/dto/request/PapagoRequest.java
@@ -0,0 +1,10 @@
+package com.example.back.domain.papago.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class PapagoRequest {
+    private String text;
+}
